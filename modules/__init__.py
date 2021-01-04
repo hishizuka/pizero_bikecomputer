eaa74951a50bf8ca7b5e47006b27b94d8f37fc98
hishizuka <xiuxing@poplar.ocn.ne.jp>
2021-01-04 22:32:11 +0900

    add headless mode

--- modules/__init__.py
+++ modules/__init__.py
@@ -11,18 +11,3 @@ t2 = datetime.datetime.now()
 print("\tlogger_core :", (t2-t1).total_seconds(),"sec")
 t1 = t2
 
-from . import gui_pyqt
-t2 = datetime.datetime.now()
-print("\tgui_pyqt :", (t2-t1).total_seconds(),"sec")
-t1 = t2
-
-#from . import gui_qml
-#t2 = datetime.datetime.now()
-#print("\tgui_qml :", (t2-t1).total_seconds(),"sec")
-#t1 = t2
-
-#from . import gui_kivy
-#t2 = datetime.datetime.now()
-#print("\tgui_kivy :", (t2-t1).total_seconds(),"sec")
-#t1 = t2
-
--- pizero_bikecomputer.py
+++ pizero_bikecomputer.py
@@ -7,6 +7,11 @@ t = datetime.datetime.now()
 import modules
 print("Loading modules... done :", (datetime.datetime.now()-t).total_seconds(),"sec")
 
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(filename='example.log', level=logging.DEBUG)
+
+
 def main():
 
   time_profile = [datetime.datetime.now(),] #for time profile
@@ -24,17 +29,20 @@ def main():
   print("\tconfig/logger:", sec_diff)
 
   gui = None
-  if config.G_GUI_MODE == "PyQt":
+  if config.G_HEADLESS:
+    pass
+  elif config.G_GUI_MODE == "PyQt":
     print("running in PyQt...")
-    gui = modules.gui_pyqt.GUI_PyQt(config)
+    t1 = datetime.datetime.now()
+    from modules import gui_pyqt
+    print("\tgui_pyqt :", (datetime.datetime.now()-t1).total_seconds(),"sec")
+    gui = gui_pyqt.GUI_PyQt(config)
   #elif config.G_GUI_MODE == "QML":
   #  print("running in QML...")
   #  gui = modules.gui_qml.GUI_QML(config)
   #elif config.G_GUI_MODE == "kivy":
   #  print("running in kivy...")
   #  gui = modules.gui_kivy.GUI_Kivy(config)
-
-  print("Bye")
 
 
 if __name__ == "__main__":
