5e318773034b379c2bdb217a3ca450836beb028d
hishizuka <xiuxing@poplar.ocn.ne.jp>
2021-04-03 10:45:52 +0900

    202104 update: improve(use sqlite3 command)

--- modules/logger/logger_csv.py
+++ modules/logger/logger_csv.py
@@ -1,6 +1,7 @@
 import sqlite3
 import time
 import datetime
+import shutil
 
 from .logger import Logger
 
@@ -22,14 +23,14 @@ class LoggerCsv(Logger):
     start_date = None
     start_date_str = "errordate"
     ## SQLite
-    con = sqlite3.connect(self.config.G_LOG_DB)
+    #con = sqlite3.connect(self.config.G_LOG_DB)
+    con = sqlite3.connect(self.config.G_LOG_DB, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+    sqlite3.dbapi2.converters['DATETIME'] = sqlite3.dbapi2.converters['TIMESTAMP']
     cur = con.cursor()
-    cur.execute("SELECT timestamp,MAX(timestamp) FROM BIKECOMPUTER_LOG LIMIT 1")
-    #cur.execute("SELECT timestamp FROM BIKECOMPUTER_LOG LIMIT 1")
+    cur.execute("SELECT timestamp, MIN(timestamp) FROM BIKECOMPUTER_LOG")
     first_row = cur.fetchone()
     if first_row != None:
-      start_date = self.config.datetime_myparser(first_row[0])
-      start_date_str = start_date.strftime("%Y%m%d%H%M%S")
+      start_date = first_row[0]
     else:
       return False
     
@@ -38,22 +39,31 @@ class LoggerCsv(Logger):
     self.config.G_LOG_START_DATE = startdate_local.strftime("%Y%m%d%H%M%S")
     filename = self.config.G_LOG_DIR + self.config.G_LOG_START_DATE + ".csv"
 
-    #file open
-    f = open(filename, "w", encoding="UTF-8")
-    # get Lap Records
     r = "\
 lap,timer,timestamp,total_timer_time,heart_rate,speed,cadence,power,distance,accumulated_power,\
 position_lat,position_long,altitude,gps_altitude,gps_distance,gps_mode,gps_used_sats,gps_total_sats,\
-total_ascent,total_descent,pressure,temperature,heading,gps_track,motion,acc_x,acc_y,acc_z,\
-voltage_battery,current_battery,voltage_out,current_out,battery_percentage\
-"
-    f.write(r+"\n")
-    for row in cur.execute("SELECT %s FROM BIKECOMPUTER_LOG" % r):
-      f.write(','.join(map(str, row))+"\n")
-    
-    f.close()
-    cur.close()
-    con.close()
+total_ascent,total_descent,pressure,temperature,heading,gps_track,motion,acc_x,acc_y,acc_z"
+#voltage_battery,current_battery,voltage_out,current_out,battery_percentage\
+#"
+    #if sqlite3 command exists, use this command (much faster)
+    if(shutil.which("sh") != None and shutil.which("sqlite3")):
+      cur.close()
+      con.close()
+      sql_cmd = "sqlite3 -header -csv " + self.config.G_LOG_DB + " \'SELECT " + r + " FROM BIKECOMPUTER_LOG;\' > " + filename
+      sqlite3_cmd = ["sh", "-c", sql_cmd]
+      self.config.exec_cmd(sqlite3_cmd)
+    else:
+      #file open
+      f = open(filename, "w", encoding="UTF-8")
+
+      # get Lap Records
+      f.write(r+"\n")
+      for row in cur.execute("SELECT %s FROM BIKECOMPUTER_LOG" % r):
+        f.write(','.join(map(str, row))+"\n")
+      f.close()
+
+      cur.close()
+      con.close()
 
     #success
     return True
