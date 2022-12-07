5830a4170e986dda70bff87e1eb5bc8c7bccf978
hishizuka <12926652+hishizuka@users.noreply.github.com>
2022-12-07 21:22:11 +0900

    add new sensor

---
+++ modules/sensor/i2c/BMP581.py
@@ -0,0 +1,147 @@
+import time
+
+try:
+  #run from top directory (pizero_bikecomputer)
+  from . import i2c
+except:
+  #directly run this program
+  import i2c
+
+# https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bmp581-ds004.pdf
+# https://github.com/BoschSensortec/BMP5-Sensor-API
+# https://github.com/sparkfun/SparkFun_BMP581_Arduino_Library
+
+# Over Sampling Rate(OSR): temperature
+# OSRS_T = 0 # no oversampling
+OSRS_T = 1 # x2 oversampling
+# OSRS_T = 2 # x4 oversampling
+# OSRS_T = 3 # x8 oversampling
+# OSRS_T = 4 # x16 oversampling
+# OSRS_T = 5 # x32 oversampling
+# OSRS_T = 6 # x64 oversampling
+# OSRS_T = 7 # x128 oversampling
+
+# Over Sampling Rate(OSR): pressure
+# OSRS_P = 0 # no oversampling
+# OSRS_P = 1 # x2 oversampling
+# OSRS_P = 2 # x4 oversampling
+OSRS_P = 3 # x8 oversampling
+# OSRS_P = 4 # x16 oversampling
+# OSRS_P = 5 # x32 oversampling
+# OSRS_P = 6 # x64 oversampling
+# OSRS_P = 7 # x128 oversampling
+
+# power mode
+# POWER_MODE = 0 # standby
+POWER_MODE = 1 # Normal mode
+# POWER_MODE = 2 # Forced mode
+# POWER_MODE = 3 # Non-Stop mode
+
+# Output Data Rate(ODR)
+# ODR = 0x00 # 240Hz
+#ODR = 0x04 # 160Hz
+# ODR = 0x06 # 140Hz
+# ODR = 0x08 # 120Hz
+# ODR = 0x0C # 80Hz
+# ODR = 0x0E # 60Hz
+# ODR = 0x11 # 40Hz
+# ODR = 0x15 # 20Hz
+ODR = 0x17 # 10Hz
+# ODR = 0x18 # 5Hz
+# ODR = 0x19 # 4Hz
+# ODR = 0x1A # 3Hz
+# ODR = 0x1B # 2Hz
+# ODR = 0x1C # 1Hz
+# ODR = 0x1D # 0.500Hz
+# ODR = 0x1E # 0.250Hz
+# ODR = 0x1F # 0.125Hz
+
+# filter settings
+# value = (value_n-1 * (FILTER - 1) + raw_data) / FILTER
+# FILTER_T = 0 # OFF
+# FILTER_T = 1 # 2
+FILTER_T = 2 # 4
+# FILTER_T = 3 # 8
+# FILTER_T = 4 # 16
+# FILTER_T = 5 # 32
+# FILTER_T = 6 # 64
+# FILTER_T = 7 # 128
+
+# FILTER_P = 0 # OFF
+# FILTER_P = 1 # 2
+FILTER_P = 2 # 4
+# FILTER_P = 3 # 8
+# FILTER_P = 4 # 16
+# FILTER_P = 5 # 32
+# FILTER_P = 6 # 64
+# FILTER_P = 7 # 128
+
+CONFIG_OSR_ENABLEPRESS = (1 << 6) + (OSRS_P << 3) + OSRS_T
+CONFIG_POWER_ODR = (1<<7) + (ODR << 2) + POWER_MODE
+CONFIG_POWER_ODR_WITH_STANDBY_MODE = (1<<7) + (ODR << 2) + 0
+CONFIG_FILTER_1 =  (1 << 5) + (1 << 3) + (1 << 1) + 1
+CONFIG_FILTER_2 = (FILTER_P << 3) + FILTER_T
+
+
+class BMP581(i2c.i2c):
+
+  #address
+  SENSOR_ADDRESS = 0x47 # or 0x46
+
+  #for reset
+  RESET_ADDRESS = 0x7E
+  RESET_VALUE = 0xB6
+
+  #for reading value
+  VALUE_ADDRESS = 0x1D
+  VALUE_BYTES = 6
+
+  #for test
+  TEST_ADDRESS = 0x01 #chip_id
+  TEST_VALUE = (0x50,)
+
+  elements = ('temperature', 'pressure')
+  temperature = None
+  pressure = None
+
+  def init_sensor(self):
+  
+    #enable pressure and config OSR
+    self.bus.write_byte_data(self.SENSOR_ADDRESS, 0x36, CONFIG_OSR_ENABLEPRESS)
+    time.sleep(0.01)
+    
+    #enable pressure and config OSR
+    #self.bus.write_byte_data(self.SENSOR_ADDRESS, 0x37, CONFIG_POWER_ODR)
+    self.bus.write_byte_data(self.SENSOR_ADDRESS, 0x37, CONFIG_POWER_ODR_WITH_STANDBY_MODE)
+    time.sleep(0.01)
+
+    #IIR Filter (need to set Standby mode)
+    self.bus.write_byte_data(self.SENSOR_ADDRESS, 0x30, CONFIG_FILTER_1)
+    time.sleep(0.01)
+    self.bus.write_byte_data(self.SENSOR_ADDRESS, 0x31, CONFIG_FILTER_2)
+    time.sleep(0.01)
+    #return Normal mode
+    self.bus.write_byte_data(self.SENSOR_ADDRESS, 0x37, CONFIG_POWER_ODR)
+    time.sleep(0.01)
+
+  def read(self):
+
+    data = self.bus.read_i2c_block_data(self.SENSOR_ADDRESS, self.VALUE_ADDRESS, self.VALUE_BYTES)
+    temperature = int.from_bytes(data[0:3],'little')/65536
+    pressure = int.from_bytes(data[3:6],'little')/6400
+
+    # Output data to screen
+    self.values['pressure'] = pressure
+    self.values['temperature'] = temperature
+    self.pressure = self.values['pressure']
+    self.temperature = self.values['temperature']
+
+
+if __name__=='__main__':
+  s = BMP581()
+  while True:
+    s.read()
+    print(s.values['temperature'], s.values['pressure'])
+    time.sleep(1)
+    
+
---
+++ modules/sensor/i2c/MMC5983MA.py
@@ -0,0 +1,129 @@
+import time
+import struct
+
+import math
+import numpy as np
+
+try:
+  #run from top directory (pizero_bikecomputer)
+  from . import i2c
+except:
+  #directly run this program
+  import i2c
+
+# https://www.memsic.com/Public/Uploads/uploadfile/files/20220119/MMC5983MADatasheetRevA.pdf
+# https://www.sparkfun.com/products/19895
+
+### MMC5983MA Register definitions ###
+OUT_X_L_M       = 0x00
+OUT_X_H_M       = 0x01
+OUT_Y_L_M       = 0x02
+OUT_Y_H_M       = 0x03
+OUT_Z_L_M       = 0x04
+OUT_Z_H_M       = 0x05
+OUT_XYZ         = 0x06
+TEMP_OUT        = 0x07
+STATUS          = 0x08
+CTRL_REG0       = 0x09
+CTRL_REG1       = 0x0A
+CTRL_REG2       = 0x0B
+CTRL_REG3       = 0x0C
+
+#Output resolution
+OUTPUT_RATE = 0 # 100Hz
+#OUTPUT_RATE = 1 # 200Hz
+#OUTPUT_RATE = 2 # 400Hz
+#OUTPUT_RATE = 3 # 800Hz
+
+#Continuous Measurement Frequency
+#CM_FREQ = 0 #OFF
+#CM_FREQ = 1 #1Hz
+CM_FREQ = 2 #10Hz
+#CM_FREQ = 3 #20Hz
+#CM_FREQ = 4 #50Hz
+#CM_FREQ = 5 #100Hz
+#CM_FREQ = 6 #200Hz (require OUTPUT_RATE=200Hz)
+#CM_FREQ = 7 #1000Hz (require OUTPUT_RATE=800Hz)
+
+
+class MMC5983MA(i2c.i2c):
+
+  #address
+  SENSOR_ADDRESS = 0x30
+
+  #SOFT RESET
+  #RESET_ADDRESS = 0x0A
+  #RESET_VALUE = 0b10000000
+  
+  #RESET
+  RESET_ADDRESS = 0x09
+  RESET_VALUE = 0x10
+
+  #for reading value
+  VALUE_ADDRESS = OUT_X_L_M
+  VALUE_BYTES = 6
+  VALUE_PATTERN = struct.Struct('>HHH')
+
+  #for test
+  TEST_ADDRESS = 0x2F
+  TEST_VALUE = (0b00110000,)
+  
+  elements = ()
+  elements_vec = ('mag',)
+  
+  # https://learn.sparkfun.com/tutorials/qwiic-9dof---ism330dhcx-mmc5983ma-hookup-guide#hardware-overview
+  #Z Axis is opposite to ISM330DHCX
+  z_sign = -1
+  #acc_factor = ACCEL_SCALE/math.pow(2, 15)
+
+  def reset_value(self):
+    for key in self.elements:
+      self.values[key] = np.nan
+    for key in self.elements_vec:
+      self.values[key] = [0] * 3
+  
+  def init_sensor(self):
+
+    #Set
+    self.bus.write_byte_data(self.SENSOR_ADDRESS, CTRL_REG0, 0x08)
+    time.sleep(0.01)
+    
+    #Auto_SR_en & INT_meas_done_en
+    self.bus.write_byte_data(self.SENSOR_ADDRESS, CTRL_REG0, 0x20 | 0x04)
+    time.sleep(0.01)
+    
+    #set output rate
+    self.bus.write_byte_data(self.SENSOR_ADDRESS, CTRL_REG1, OUTPUT_RATE)
+    time.sleep(0.01)
+    #enable Continuous Measurement mode and set frequency
+    self.bus.write_byte_data(self.SENSOR_ADDRESS, CTRL_REG2, (1<<3) | CM_FREQ)
+    time.sleep(0.01)
+
+  def read(self):
+    self.read_mag()
+
+  def read_mag(self):
+    #Read the magnetomter and return the raw x, y and z magnetic readings as a vector.
+    status = self.bus.read_i2c_block_data(self.SENSOR_ADDRESS, STATUS, 1)
+    self.bus.write_byte_data(self.SENSOR_ADDRESS, STATUS, status[0]&0x01)
+
+    mag_xyz = self.bus.read_i2c_block_data(self.SENSOR_ADDRESS, OUT_XYZ, 1)
+    data = self.bus.read_i2c_block_data(self.SENSOR_ADDRESS, self.VALUE_ADDRESS, self.VALUE_BYTES)
+    mag_raw = list(map(lambda x: x << 2, self.VALUE_PATTERN.unpack(bytearray(data))))
+    self.values['mag'][0] = (((mag_raw[0] | (((mag_xyz[0] & 0xC0) >> 6) & 0x3)) -131072)/16384)*100
+    self.values['mag'][1] = (((mag_raw[1] | (((mag_xyz[0] & 0x30) >> 4) & 0x3)) -131072)/16384)*100
+    self.values['mag'][2] = self.z_sign*(((mag_raw[2] | (((mag_xyz[0] & 0x03) >> 2) & 0x3)) -131072)/16384)*100
+
+
+if __name__=="__main__":
+  l = MMC5983MA()
+  while True:
+    l.read()
+    print("{:+.1f}, {:+.1f}, {:+.1f}".format(
+      l.values['mag'][0],
+      l.values['mag'][1],
+      l.values['mag'][2],
+      ))
+    time.sleep(0.1)
+
+
