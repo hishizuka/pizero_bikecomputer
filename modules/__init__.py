e8273d8d626cb732564605dc565afda07c347e8a
hishizuka <12926652+hishizuka@users.noreply.github.com>
2022-12-07 21:43:51 +0900

    delete code

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,13 +1,2 @@
-import datetime
 
-t1 = datetime.datetime.now()
-from . import config
-t2 = datetime.datetime.now()
-print("\tconfig :", (t2-t1).total_seconds(),"sec")
-t1 = t2
-
-from . import logger_core
-t2 = datetime.datetime.now()
-print("\tlogger_core :", (t2-t1).total_seconds(),"sec")
-t1 = t2
 
