5005108abf99b9deb4f3ae9df085fa8c6d5151be
hishizuka <12926652+hishizuka@users.noreply.github.com>
2023-05-26 21:49:29 +0900

    add

---
+++ modules/helper/ble_gatt_server.py
@@ -0,0 +1,134 @@
+import os
+import asyncio
+import json
+import re
+import traceback
+import datetime
+
+from bluez_peripheral.gatt.service import Service
+from bluez_peripheral.gatt.characteristic import characteristic, CharacteristicFlags as CharFlags
+from bluez_peripheral.util import *
+from bluez_peripheral.advert import Advertisement
+from bluez_peripheral.agent import NoIoAgent
+
+
+class GadgetbridgeService(Service):
+
+  service_uuid =            '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
+  rx_characteristic_uuid =  '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
+  tx_characteristic_uuid =  '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
+  
+  config = None
+  bus = None
+
+  status = False
+  gps_status = False
+  value = bytearray()
+  value_extend = False
+
+  timestamp_str = ""
+  timestamp_status = False
+  timestamp_done = False
+
+  def __init__(self, config):
+    self.config = config
+    super().__init__(self.service_uuid, True)
+  
+  async def quit(self):
+    if not self.status and self.bus != None:
+      self.bus.disconnect()
+
+  #direct access from central
+  @characteristic(tx_characteristic_uuid, CharFlags.NOTIFY | CharFlags.READ)
+  def tx_characteristic(self, options):
+    return bytes(self.config.G_PRODUCT, "utf-8")
+
+  #notice to central
+  def send_message(self, value):
+    #debug print
+    self.tx_characteristic.changed(bytes(value+"\\n\n", "utf-8"))
+
+  #recieve from central
+  @characteristic(rx_characteristic_uuid, CharFlags.WRITE).setter
+  def rx_characteristic(self, value, options):
+    #debug
+    #try:
+    #  print(value.decode())
+    #except:
+    #  print(value)
+    
+    if value[0] == 0x10:
+      self.value = bytearray()
+      self.value_extend = True
+
+      self.timestamp_status = False
+      if (not self.timestamp_done or self.timestamp_status) and value[1:8].decode() == "setTime":
+        self.timestamp_str = value.decode()[1:]
+        self.timestamp_status = True
+
+    elif self.timestamp_status:
+      self.timestamp_str += value.decode()
+
+      res = re.match('^setTime\((\d+)\);E.setTimeZone\((\S+)\)', self.timestamp_str)
+      if res != None:
+        self.timestamp_done = True
+        self.timestamp_status = False
+        time_diff = datetime.timedelta(hours=float(res.group(2)))
+        self.config.logger.sensor.sensor_gps.set_timediff_from_utc(time_diff)
+        utctime = datetime.datetime.fromtimestamp(int(res.group(1))) - time_diff
+        self.config.logger.sensor.sensor_gps.get_utc_time_from_datetime(utctime)
+      
+    if self.value_extend:
+      self.value.extend(bytearray(value))
+    #self.value.extend(bytearray(value))
+
+    #for gadgetbridge JSON message
+    if len(self.value) > 8 and self.value[-3] == 0x7D and self.value[-2] == 0x29 and self.value[-1] == 0x0A:
+      text_mod = re.sub('(\w+):("?\w*"?)', '"\\1":\\2', self.value.decode().strip()[5:-2])
+      message = {}
+      try:
+        message = json.loads('{' + text_mod + '}', strict=False)
+      except:
+        print("failed to load json")
+        print(traceback.print_exc())
+        print(text_mod)
+        try:
+          message = json.loads('{' + text_mod + '"}', strict=False)
+        except:
+          print("failed to load json (retry)")
+
+      if 't' in message and message['t'] == 'notify' and 'title' in message and 'body' in message:
+        if len(message['title'] > 30):
+          message['title'] = message['title'][0:30] + "..."
+        if len(message['body'] > 50):
+          message['body'] = message['body'][0:50] + "..."
+        asyncio.create_task(self.config.gui.show_message(message['title'], message['body']))
+        print("success: ", message)
+      elif 't' in message and message['t'] == 'find' and 'n' in message and message['n']:
+        self.config.gui.show_dialog_ok_only(fn=None, title="Gadgetbridge")
+      elif 't' in message and message['t'] == 'gps':
+        asyncio.create_task(self.config.logger.sensor.sensor_gps.update_GB(message))
+
+      self.value_extend = False
+
+  async def on_off_uart_service(self):
+    if self.status:
+      self.bus.disconnect()
+    else:
+      self.bus = await get_message_bus()
+      await self.register(self.bus)
+      agent = NoIoAgent()
+      await agent.register(self.bus)
+      adapter = await Adapter.get_first(self.bus)
+      advert = Advertisement(self.config.G_PRODUCT, [self.service_uuid], 0, 60)
+      await advert.register(self.bus, adapter)
+    
+    self.status = not self.status
+
+  def on_off_gadgetbridge_gps(self):
+    if not self.gps_status:
+      self.send_message('{t:"gps_power", status:true}')
+    else:
+      self.send_message('{t:"gps_power", status:false}')
+    self.gps_status = not self.gps_status
+
---
+++ modules/helper/bt_pan.py
@@ -0,0 +1,193 @@
+import traceback
+
+HAS_DBUS_NEXT = False
+HAS_DBUS = False
+
+try:
+  from dbus_next import BusType, DBusError
+  from dbus_next.service import ServiceInterface, method, signal, dbus_property
+  from dbus_next.aio.message_bus import MessageBus
+  from dbus_next import Variant
+  HAS_DBUS_NEXT = True
+except:
+  pass
+
+try:
+  if not HAS_DBUS_NEXT:
+    import dbus
+    HAS_DBUS = True
+except:
+  pass
+
+
+class BTPan():
+  bus = None
+  is_available = False
+  devices = {}
+
+  obj_bluez = "org.bluez"
+  obj_object_manager = "org.freedesktop.DBus.ObjectManager"
+  obj_properties = "org.freedesktop.DBus.Properties"
+  obj_device = "org.bluez.Device1"
+  obj_service =  "org.bluez.Network1"
+  path_bluez = '/org/bluez'
+
+  service_uuid = "nap"
+  remote_addr = ""
+  interface = None
+
+
+class BTPanDbusNext(BTPan):
+  
+  async def check_dbus(self):
+    try:
+      self.bus = await MessageBus(bus_type=BusType.SYSTEM).connect()
+      introspection = await self.bus.introspect(self.obj_bluez, self.path_bluez)
+      self.is_available = True
+    except:
+      pass
+    
+  async def find_bt_pan_devices(self):
+    res = {}
+    self.devices = {}
+
+    proxy_object = self.bus.get_proxy_object(self.obj_bluez, "/", await self.bus.introspect(self.obj_bluez, "/"))
+    manager = proxy_object.get_interface(self.obj_object_manager)
+    objs = await manager.call_get_managed_objects()
+    for path, ifaces in objs.items():
+      if self.obj_service in ifaces.keys():
+        proxy_object = self.bus.get_proxy_object(self.obj_bluez, path, await self.bus.introspect(self.obj_bluez, path))
+        device = proxy_object.get_interface(self.obj_device)
+        address = await device.get_address()
+        res[await device.get_name()] = address
+        self.devices[address] = path
+    
+    return res
+  
+  async def initialize_device(self, remote_addr):
+    try:
+      if self.remote_addr != remote_addr:
+        obj_remote_addr =  self.devices[remote_addr]
+        proxy_object = self.bus.get_proxy_object(self.obj_bluez, obj_remote_addr, await self.bus.introspect(self.obj_bluez, obj_remote_addr))
+        self.interface = proxy_object.get_interface(self.obj_service)
+        self.remote_addr = remote_addr
+    except:
+      print(traceback.print_exc())
+      return False
+    return True
+  
+  async def connect_tethering(self, remote_addr):
+    connected = None
+
+    if not await self.initialize_device(remote_addr):
+      return False
+
+    for n in range(2):
+      try:
+        await self.interface.call_connect(self.service_uuid)
+      except DBusError as e:
+        #error = e.get_dbus_name()
+        print(e)
+        await asyncio.sleep(1)
+      else:
+        break
+    connected = await self.interface.get_connected()
+    
+    return connected
+
+  async def disconnect_tethering(self, remote_addr):
+    connected = None
+
+    if not await self.initialize_device(remote_addr):
+      return False
+
+    try:
+      await self.interface.call_disconnect()
+    except DBusError as e:
+      #error = e.get_dbus_name()
+      print(e)
+    connected = await self.interface.get_connected()
+    
+    return connected
+
+
+#based on bluez(https://github.com/bluez/bluez) test/test-network
+class BTPanDbus(BTPan):
+  
+  async def check_dbus(self):
+    try:
+      self.bus = dbus.SystemBus()
+      proxy = self.bus.get_name_owner(self.obj_bluez)
+      self.is_available = True
+    except:
+      pass
+
+  def get_managed_objects(self):
+    manager = dbus.Interface(self.bus.get_object(self.obj_bluez, "/"), self.obj_object_manager)
+    return manager.GetManagedObjects()
+  
+  async def find_bt_pan_devices(self):
+    objects = self.get_managed_objects()
+    res = {}
+    self.devices = {}
+
+    for path, ifaces in objects.items():
+      for i in ifaces:
+        if self.obj_service in i:
+          device = ifaces.get(self.obj_device)
+          res[device["Name"]] = device["Address"]
+          self.devices[device["Address"]] = path
+    return res
+
+  def prop_get(self, obj, k, iface=None):
+    if iface is None:
+      iface = obj.dbus_interface
+    return obj.Get(iface, k, dbus_interface=self.obj_properties)
+
+  def initialize_device(self, remote_addr):
+    try:
+      if self.remote_addr != remote_addr:
+        self.interface = dbus.Interface(self.bus.get_object(self.obj_bluez, self.devices[remote_addr]), self.obj_service)
+        self.remote_addr = remote_addr
+    except:
+      import traceback
+      print(traceback.print_exc())
+      return False
+    return True
+
+  async def connect_tethering(self, remote_addr):
+    connected = None
+    if not self.initialize_device(remote_addr):
+      return False
+    
+    for n in range(2):
+      try:
+        iface = self.interface.Connect(self.service_uuid)
+      except dbus.exceptions.DBusException as e:
+        error = e.get_dbus_name()
+        print(error)
+        await asyncio.sleep(1)
+        #'org.freedesktop.DBus.Error.NoReply'
+        #'org.bluez.Error.Failed'
+      else:
+        break
+    connected = self.prop_get(self.interface, 'Connected')
+    
+    return connected
+
+  async def disconnect_tethering(self, remote_addr):
+    connected = None
+    if not self.initialize_device(remote_addr):
+      return False
+    
+    try:
+      self.interface.Disconnect()
+    except dbus.exceptions.DBusException as e:
+      error = e.get_dbus_name()
+      print(error)
+      #'org.bluez.Error.NotConnected'
+      #'org.bluez.Error.Failed'
+    connected = self.prop_get(self.interface, 'Connected')
+
+    return connected
+
