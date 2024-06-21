522376dffdf540d1f0c55345c18a03608417c02a
hishizuka <12926652+hishizuka@users.noreply.github.com>
2024-06-21 14:15:30 +0900

    Add vertical layouts, smaller font size for units

--- modules/pyqt/pyqt_item.py
+++ modules/pyqt/pyqt_item.py
@@ -14,7 +14,7 @@ class ItemLabel(QtWidgets.QLabel):
         return f"""
             border-width: 0px {right_border_width} 0px 0px;
             border-style: solid;
-            border-color: #CCCCCC;
+            border-color: #AAAAAA;
         """
 
     def __init__(self, right, *__args):
@@ -26,7 +26,7 @@ class ItemLabel(QtWidgets.QLabel):
 
 class ItemValue(QtWidgets.QLabel):
     bottom = False
-    right = False
+    right = False ## not need
 
     @property
     def STYLES(self):
@@ -35,7 +35,7 @@ class ItemValue(QtWidgets.QLabel):
         return f"""
             border-width: 0px {right_border_width} {bottom_border_width} 0px;
             border-style: solid;
-            border-color: #CCCCCC;
+            border-color: #AAAAAA;
         """
 
     def __init__(self, right, bottom, *__args):
@@ -54,8 +54,10 @@ class Item(QtWidgets.QVBoxLayout):
     label = None
     value = None
     name = ""
+    font_size_unit = 0
+    font_size_unit_set = False
 
-    def __init__(self, config, name, font_size, bottom_flag, right_flag, *args):
+    def __init__(self, config, name, font_size, right_flag, bottom_flag, *args):
         super().__init__(*args)
         self.config = config
         self.name = name
@@ -64,13 +66,17 @@ class Item(QtWidgets.QVBoxLayout):
         self.setSpacing(0)
 
         self.label = ItemLabel(right_flag, name)
-        self.value = ItemValue(right_flag, bottom_flag)
+        self.value_layout = QtWidgets.QHBoxLayout()
+        self.value_layout.setContentsMargins(0, 0, 0, 0)
+        self.value_layout.setSpacing(0)
 
-        self.itemformat = self.config.gui.gui_config.G_ITEM_DEF[name][0]
+        self.value = ItemValue(right_flag, bottom_flag)
+        self.itemformat = self.config.gui.gui_config.G_ITEM_DEF[name][0][0]
+        self.unittext = self.config.gui.gui_config.G_ITEM_DEF[name][0][1]
 
         self.addWidget(self.label)
         self.addWidget(self.value)
-
+        
         self.update_font_size(font_size)
         self.update_value(np.nan)
 
@@ -81,18 +87,12 @@ class Item(QtWidgets.QVBoxLayout):
             self.value.setText(value)
         elif np.isnan(value):
             self.value.setText("-")
-        elif self.name.startswith("Speed"):
-            self.value.setText(self.itemformat.format(value * 3.6))  # m/s to km/h
-        elif "SPD" in self.name:
-            self.value.setText(self.itemformat.format(value * 3.6))  # m/s to km/h
-        elif "Dist" in self.name:
-            self.value.setText(self.itemformat.format(value / 1000))  # m to km
-        elif "DIST" in self.name:
-            self.value.setText(self.itemformat.format(value / 1000))  # m to km
-        elif "Work" in self.name:
-            self.value.setText(self.itemformat.format(value / 1000))  # j to kj
-        elif "WRK" in self.name:
-            self.value.setText(self.itemformat.format(value / 1000))  # j to kj
+        elif self.name.startswith("Speed") or "SPD" in self.name:
+            self.value.setText(f"{(value * 3.6):{self.itemformat}}")  # m/s to km/h
+        elif "Dist" in self.name or "DIST" in self.name:
+            self.value.setText(f"{(value / 1000):{self.itemformat}}")  # m to km
+        elif "Work" in self.name or "WRK" in self.name:
+            self.value.setText(f"{(value / 1000):{self.itemformat}}")  # j to kj
         elif (
             "Grade" in self.name or "Glide" in self.name
         ) and self.config.G_STOPWATCH_STATUS != "START":
@@ -106,14 +106,28 @@ class Item(QtWidgets.QVBoxLayout):
         elif self.itemformat == "time":
             self.value.setText(time.strftime("%H:%M"))
         else:
-            self.value.setText(self.itemformat.format(value))
+            self.value.setText(f"{value:{self.itemformat}}")
+        
+        if self.unittext != "":
+            if self.font_size_unit_set:
+                self.value.setText(
+                    self.value.text()
+                    + f"<span style='font-size: {self.font_size_unit}px;'> {self.unittext}</span>"
+                    )
+            else:
+                self.value.setText(self.value.text() + f"<font size=small> {self.unittext}</font>")
 
     def update_font_size(self, font_size):
+        if not self.font_size_unit_set and self.font_size_unit != 0:
+            self.font_size_unit_set = True
+        self.font_size_unit = int(font_size * 0.7)
+        
         for text, fsize in zip(
             [self.label, self.value], [int(font_size * 0.66), font_size]
         ):
             q = text.font()
             q.setPixelSize(fsize)
+            # q.setLetterSpacing(QtGui.QFont.SpacingType.PercentageSpacing, 95)
             # q.setStyleStrategy(QtGui.QFont.NoSubpixelAntialias) # avoid subpixel antialiasing on the fonts if possible
             # q.setStyleStrategy(QtGui.QFont.NoAntialias) # don't antialias the fonts
             text.setFont(q)
