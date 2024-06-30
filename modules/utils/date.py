e3e714678e01788b06c0c7e50e41dcaa8fc02342
hishizuka <12926652+hishizuka@users.noreply.github.com>
2024-06-30 17:31:41 +0900

    bug fix

--- modules/utils/date.py
+++ modules/utils/date.py
@@ -1,32 +1,25 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from logger import app_logger
 
 
 # replacement of dateutil.parser.parse
 def datetime_myparser(ts):
-    if len(ts) == 14:
-        # 20190322232414 / 14 chars
-        dt = datetime(
-            int(ts[0:4]),  # %Y
-            int(ts[4:6]),  # %m
-            int(ts[6:8]),  # %d
-            int(ts[8:10]),  # %H
-            int(ts[10:12]),  # %M
-            int(ts[12:14]),  # %s
-        )
-        return dt
-    elif 24 <= len(ts) <= 26 or len(ts) == 32:
-        # 2019-03-22T23:24:14.280604        / 26 chars
-        # 2019-09-30T12:44:55.000Z          / 24 chars
-        # 2024-06-24 00:58:54.801928+00:00  / 32 chars
-        dt = datetime(
-            int(ts[0:4]),  # %Y
-            int(ts[5:7]),  # %m
-            int(ts[8:10]),  # %d
-            int(ts[11:13]),  # %H
-            int(ts[14:16]),  # %M
-            int(ts[17:19]),  # %s
-        )
-        return dt
-    app_logger.error(f"Could not parse date {ts} {len(ts)}")
+    if len(ts) not in [24, 26, 32]:
+        app_logger.error(f"Could not parse date {ts} {len(ts)}")
+        return None
+    
+    # 2024-06-24 00:58:54.801928+00:00  / 32 chars (from log.db)
+    # 2019-09-30T12:44:55.000Z          / 24 chars (obsolete)
+    # 2019-03-22T23:24:14.280604        / 26 chars (obsolete)
+    dt = datetime(
+        int(ts[0:4]),  # %Y
+        int(ts[5:7]),  # %m
+        int(ts[8:10]),  # %d
+        int(ts[11:13]),  # %H
+        int(ts[14:16]),  # %M
+        int(ts[17:19]),  # %s
+        tzinfo=timezone.utc,
+    )
+    return dt
+    
