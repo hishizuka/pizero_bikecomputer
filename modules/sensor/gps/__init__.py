dd5e50f2bec122c4ecd6e8b79b93c47aaae0a917
hishizuka <12926652+hishizuka@users.noreply.github.com>
2025-10-19 07:05:39 +0900

    drop i2c gps and NMEA

--- modules/sensor/gps/__init__.py
+++ modules/sensor/gps/__init__.py
@@ -1,20 +1,12 @@
 from modules.app_logger import app_logger
 
-
-from .adafruit_uart import _SENSOR_GPS_ADAFRUIT_UART, Adafruit_GPS
 from .gpsd import _SENSOR_GPS_GPSD, _SENSER_GPS_STR, GPSD
-from .i2c import _SENSOR_GPS_I2C, GPS_I2C
-
-# we always have a non-null GPS sensor, but it won't generate data unless told to do so with G_DUMMY_OUTPUT
-from .dummy import Dummy_GPS
 
 if _SENSOR_GPS_GPSD:
     SensorGPS = GPSD
-elif _SENSOR_GPS_I2C:
-    SensorGPS = GPS_I2C
-elif _SENSOR_GPS_ADAFRUIT_UART:
-    SensorGPS = Adafruit_GPS
 else:
+    # we always have a non-null GPS sensor, but it won't generate data unless told to do so with G_DUMMY_OUTPUT
+    from .dummy import Dummy_GPS
     SensorGPS = Dummy_GPS
 
 app_logger.info(f"  GPS ({SensorGPS.__name__}" + (f"/{_SENSER_GPS_STR}" if _SENSOR_GPS_GPSD else "") + ")")
