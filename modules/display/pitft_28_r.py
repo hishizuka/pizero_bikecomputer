f25600a88977f1c41ca02c826ae4d2849c5e05bf
hishizuka <12926652+hishizuka@users.noreply.github.com>
2026-01-30 07:18:19 +0900

    Trixie support for PiTFT

--- modules/display/pitft_28_r.py
+++ modules/display/pitft_28_r.py
@@ -8,32 +8,41 @@ app_logger.info(f"PiTFT 2.8(r): {_SENSOR_DISPLAY}")
 
 
 class PiTFT28r(Display):
-    # PiTFT actually needs max brightness under sunlights, so there is no implementation of AUTO_BACKLIGHT
-    # There's also only two states enabled or disabled
-    brightness_index = 1  # we are on by default
-    brightness_table = [0, 100]
+    brightness_table = [0, 10, 100]
+    brightness = 100
+    brightness_index = len(brightness_table) - 1
+    minimum_brightness = 10
+
+    has_backlight = True
+    allow_auto_backlight = False
+    use_auto_backlight = False
+
+    _BACKLIGHT_PATH = "/sys/class/backlight/backlight/brightness"
 
     size = (320, 240)
+
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.brightness_index = len(self.brightness_table) - 1
+        self.brightness = -1
+        self.set_brightness(self.brightness_table[self.brightness_index])
 
     def quit(self):
         self.set_brightness(0)
 
     def set_brightness(self, b):
-        if b == 0:
-            exec_cmd(
-                [
-                    "sudo",
-                    "sh",
-                    "-c",
-                    r"echo 0 > /sys/class/backlight/soc\:backlight/brightness",
-                ]
-            )
-        elif b == 100:
-            exec_cmd(
-                [
-                    "sudo",
-                    "sh",
-                    "-c",
-                    r"echo 1 /sys/class/backlight/soc\:backlight/brightness",
-                ]
-            )
+        if b == self.brightness:
+            return
+
+        value = int(max(0, min(100, b)) * 255 / 100)
+        exec_cmd(
+            [
+                "sudo",
+                "sh",
+                "-c",
+                f"echo {value} > {self._BACKLIGHT_PATH}",
+            ],
+            cmd_print=False
+        )
+        self.brightness = b
