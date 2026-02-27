ae6119997a399db989c12b375ace838c538b651a
hishizuka <12926652+hishizuka@users.noreply.github.com>
2026-02-28 08:09:43 +0900

    modify

--- modules/sensor/i2c/cython/bhi360_shuttle_board_3/build.py
+++ modules/sensor/i2c/cython/bhi360_shuttle_board_3/build.py
@@ -14,7 +14,10 @@ print(f"{bhi3_s.status=}")
 
 while True:
     try:
-        print(f"heading={bhi3_s.heading}, pitch={bhi3_s.pitch}, roll={bhi3_s.roll}, acc_rms_norm={bhi3_s.acc_rms_norm}, pressure={bhi3_s.pressure}, temperature={bhi3_s.temperature}, humidity={bhi3_s.humidity}")
+        heading_deg = f"{bhi3_s.heading:.0f}°"
+        pitch_deg = f"{bhi3_s.pitch:.0f}°"
+        roll_deg = f"{bhi3_s.roll:.0f}°"
+        print(f"heading={heading_deg}, pitch={pitch_deg}, roll={roll_deg}, acc_rms_norm={bhi3_s.acc_rms_norm}, pressure={bhi3_s.pressure}, temperature={bhi3_s.temperature}, humidity={bhi3_s.humidity}")
         print(f"moving={bhi3_s.moving}, acc={bhi3_s.acc}")
         print()
         time.sleep(1)
