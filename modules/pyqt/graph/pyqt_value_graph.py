dfb76c44ebb5e6a8963be0b2186f6970e5f3b385
hishizuka <12926652+hishizuka@users.noreply.github.com>
2026-03-06 22:52:23 +0900

    Refactor PyQt value graphs to reuse plot items

--- modules/pyqt/graph/pyqt_value_graph.py
+++ modules/pyqt/graph/pyqt_value_graph.py
@@ -7,6 +7,17 @@ HOLIZONTAL_ITEMS = 4
 VERTICAL_ITEMS = 2
 
 
+def build_item_layout(elements, cols, widget_name):
+    item_layout = {}
+    for i, element in enumerate(elements):
+        item_layout[element] = (i // cols, i % cols)
+
+    plot_y = (len(elements) + cols - 1) // cols
+    item_layout[widget_name] = (plot_y, 0, -1, -1)
+
+    return item_layout, plot_y
+
+
 class PerformanceGraphWidget(ScreenWidget):
     elements = ("Power", "HR", "W'bal(Norm)", "LapTime")
     item_layout = {}
@@ -16,33 +27,36 @@ class PerformanceGraphWidget(ScreenWidget):
     brush = pg.mkBrush(color=(0, 255, 255))
     # pen2 = pg.mkPen(color=(255,255,255,0), width=0.01) #transparent and thin line
     pen1 = pg.mkPen(color=(255, 255, 255), width=0.01)  # transparent and thin line
+    legend_pen1 = pg.mkPen(color=(0, 255, 255), width=3)
     # for HR, wbal
     pen2 = pg.mkPen(color=(255, 0, 0), width=2)
 
     def __init__(self, parent, config):
         s = config.display.resolution
         cols = VERTICAL_ITEMS if s[0] < s[1] else HOLIZONTAL_ITEMS
-        for i, e in enumerate(self.elements):
-            self.item_layout[e] = (i // cols, i % cols)
-        max_height = len(self.elements) // cols
-        self.item_layout[self.__class__.__name__] = (max_height, 0, -1, -1)
+        self.item_layout, plot_y = build_item_layout(
+            self.elements, cols, self.__class__.__name__
+        )
         self.plot_x = 0
-        self.plot_y = max_height
+        self.plot_y = plot_y
 
         self.display_item = config.G_GUI_PERFORMANCE_GRAPH_DISPLAY_ITEM
         self.item = {
             "POWER": {
                 "name": "POWER",
+                "legend_name": "Power",
                 "graph_key": "power_graph",
                 "yrange": [config.G_GUI_MIN_POWER, config.G_GUI_MAX_POWER],
             },
             "HR": {
                 "name": "HR",
+                "legend_name": "HR",
                 "graph_key": "hr_graph",
                 "yrange": [config.G_GUI_MIN_HR, config.G_GUI_MAX_HR],
             },
             "W_BAL": {
                 "name": "W_BAL",
+                "legend_name": "W'bal(Norm)",
                 "graph_key": "w_bal_graph",
                 "yrange": [config.G_GUI_MIN_W_BAL, config.G_GUI_MAX_W_BAL],
             },
@@ -50,6 +64,9 @@ class PerformanceGraphWidget(ScreenWidget):
         self.plot_data_x1 = []
         for i in range(config.G_GUI_PERFORMANCE_GRAPH_DISPLAY_RANGE + 1):
             self.plot_data_x1.append(i)
+        self.power_bar_x0 = np.asarray(self.plot_data_x1[:-1], dtype=np.float32)
+        self.power_bar_x1 = np.asarray(self.plot_data_x1[1:], dtype=np.float32)
+        self.empty_bar_data = np.asarray([], dtype=np.float32)
 
         super().__init__(parent, config, self.item_layout)
 
@@ -65,6 +82,34 @@ class PerformanceGraphWidget(ScreenWidget):
         self.p1.scene().addItem(self.p2)
         self.p1.getAxis("right").linkToView(self.p2)
         self.p2.setXLink(self.p1)
+        self.p1.vb.sigResized.connect(self._sync_overlay_view)
+
+        self.power_view = self.p1.vb
+        self.power_bar_item = pg.BarGraphItem(
+            x0=self.empty_bar_data,
+            x1=self.empty_bar_data,
+            height=self.empty_bar_data,
+            brush=self.brush,
+            pen=self.pen1,
+        )
+        self.power_bar_item.setZValue(1)
+        self.power_view.addItem(self.power_bar_item)
+        self.secondary_curve = pg.PlotCurveItem(pen=self.pen2, connect="finite")
+        self.p2.addItem(self.secondary_curve)
+        self.legend = self.p1.addLegend(offset=(8, 8))
+        self.legend_power_item = pg.PlotCurveItem(
+            pen=self.legend_pen1,
+            connect="finite",
+        )
+        self.legend.addItem(
+            self.legend_power_item,
+            self.item[self.display_item[0]]["legend_name"],
+        )
+        self.legend.addItem(
+            self.secondary_curve,
+            self.item[self.display_item[1]]["legend_name"],
+        )
+        self.legend.setZValue(1000)
 
         plot.setXRange(0, self.config.G_GUI_PERFORMANCE_GRAPH_DISPLAY_RANGE)
         self.p1.setYRange(*self.item[self.display_item[0]]["yrange"])
@@ -78,6 +123,12 @@ class PerformanceGraphWidget(ScreenWidget):
         self.p1.setZValue(-100)
 
         self.layout.addWidget(plot, self.plot_y, self.plot_x, -1, -1)
+        self._sync_overlay_view()
+
+    def _sync_overlay_view(self):
+        scene_rect = self.p1.vb.sceneBoundingRect()
+        self.p2.setGeometry(scene_rect)
+        self.p2.linkedViewChanged(self.p1.vb, self.p2.XAxis)
 
     def set_font_size(self, length):
         self.font_size = int(length / 7)
@@ -97,17 +148,14 @@ class PerformanceGraphWidget(ScreenWidget):
             if False in chk:
                 all_nan[key] = False
 
-        if not all_nan[self.item[self.display_item[0]]["graph_key"]]:
-            self.p1.clear()
-
+        power_key = self.item[self.display_item[0]]["graph_key"]
+        if not all_nan[power_key]:
             # change max for power
             if self.display_item[0] == "POWER":
                 power_max = 100 * (
                     int(
                         np.nanmax(
-                            self.sensor.values["integrated"][
-                                self.item[self.display_item[0]]["graph_key"]
-                            ]
+                            self.sensor.values["integrated"][power_key]
                         )
                         / 100
                     )
@@ -117,37 +165,33 @@ class PerformanceGraphWidget(ScreenWidget):
                     self.item[self.display_item[0]]["yrange"][1] = power_max
                     self.p1.setYRange(*self.item[self.display_item[0]]["yrange"])
 
-            self.p1.addItem(
-                pg.BarGraphItem(
-                    x0=self.plot_data_x1[:-1],
-                    x1=self.plot_data_x1[1:],
-                    height=self.sensor.values["integrated"][
-                        self.item[self.display_item[0]]["graph_key"]
-                    ],
-                    brush=self.brush,
-                    pen=self.pen1,
-                )
+            power_data = np.asarray(
+                self.sensor.values["integrated"][power_key],
+                dtype=np.float32,
+            ).copy()
+            self.power_bar_item.setOpts(
+                x0=self.power_bar_x0,
+                x1=self.power_bar_x1,
+                height=power_data,
             )
+            self.power_view.update()
+        else:
+            self.power_bar_item.setOpts(
+                x0=self.empty_bar_data,
+                x1=self.empty_bar_data,
+                height=self.empty_bar_data,
+            )
+            self.power_view.update()
 
-        # if not all_nan['hr_graph']:
-        if not all_nan[self.item[self.display_item[1]]["graph_key"]]:
-            self.p2.clear()
-            self.p2.setGeometry(self.p1.vb.sceneBoundingRect())
-            self.p2.linkedViewChanged(self.p1.vb, self.p2.XAxis)
-            # for HR
-            self.p2.addItem(
-                pg.PlotCurveItem(
-                    # self.sensor.values['integrated']['hr_graph'],
-                    self.sensor.values["integrated"][
-                        self.item[self.display_item[1]]["graph_key"]
-                    ],
-                    pen=self.pen2,
-                )
-            )
+        secondary_key = self.item[self.display_item[1]]["graph_key"]
+        if not all_nan[secondary_key]:
+            self.secondary_curve.setData(self.sensor.values["integrated"][secondary_key])
+        else:
+            self.secondary_curve.setData([])
 
 
 class AccelerationGraphWidget(ScreenWidget):
-    elements = ("ACC_X", "ACC_Y", "ACC_Z", "M_Stat")
+    elements = ("Motion", "M_Stat")
     item_layout = {}
 
     # for acc
@@ -160,12 +204,11 @@ class AccelerationGraphWidget(ScreenWidget):
     def __init__(self, parent, config):
         s = config.display.resolution
         cols = VERTICAL_ITEMS if s[0] < s[1] else HOLIZONTAL_ITEMS
-        for i, e in enumerate(self.elements):
-            self.item_layout[e] = (i // cols, i % cols)
-        max_height = len(self.elements) // cols
-        self.item_layout[self.__class__.__name__] = (max_height, 0, -1, -1)
+        self.item_layout, plot_y = build_item_layout(
+            self.elements, cols, self.__class__.__name__
+        )
         self.plot_x = 0
-        self.plot_y = max_height
+        self.plot_y = plot_y
 
         super().__init__(parent, config, self.item_layout)
 
@@ -182,11 +225,32 @@ class AccelerationGraphWidget(ScreenWidget):
         self.p3 = pg.ViewBox()
         self.p1.scene().addItem(self.p3)
         self.p3.setXLink(self.p1)
+        self.p1.vb.sigResized.connect(self._sync_overlay_views)
+
+        self.acc_curve_x = pg.PlotCurveItem(pen=self.pen1, connect="finite")
+        self.acc_curve_y = pg.PlotCurveItem(pen=self.pen2, connect="finite")
+        self.acc_curve_z = pg.PlotCurveItem(pen=self.pen3, connect="finite")
+        self.p1.addItem(self.acc_curve_x)
+        self.p2.addItem(self.acc_curve_y)
+        self.p3.addItem(self.acc_curve_z)
+        self.legend = self.p1.addLegend(offset=(8, 8))
+        self.legend.addItem(self.acc_curve_x, "ACC X")
+        self.legend.addItem(self.acc_curve_y, "ACC Y")
+        self.legend.addItem(self.acc_curve_z, "ACC Z")
+        self.legend.setZValue(1000)
 
         plot.setXRange(0, self.config.G_GUI_ACC_TIME_RANGE)
         plot.setMouseEnabled(x=False, y=False)
 
         self.layout.addWidget(plot, self.plot_y, self.plot_x, -1, -1)
+        self._sync_overlay_views()
+
+    def _sync_overlay_views(self):
+        scene_rect = self.p1.vb.sceneBoundingRect()
+        self.p2.setGeometry(scene_rect)
+        self.p2.linkedViewChanged(self.p1.vb, self.p2.XAxis)
+        self.p3.setGeometry(scene_rect)
+        self.p3.linkedViewChanged(self.p1.vb, self.p3.XAxis)
 
     def start(self):
         self.timer.start(self.config.G_REALTIME_GRAPH_INTERVAL)
@@ -214,34 +278,25 @@ class AccelerationGraphWidget(ScreenWidget):
         if len(m):
             median = m[-1]
 
-        if not all_nan[X]:
-            self.p1.clear()
-            if median is not None:
-                self.p1.setYRange(-self.g_range, self.g_range)
+        if median is not None:
+            self.p1.setYRange(-self.g_range, self.g_range)
+            self.p2.setYRange(-self.g_range, self.g_range)
+            self.p3.setYRange(-self.g_range, self.g_range)
 
-            self.p1.addItem(pg.PlotCurveItem(v[X], pen=self.pen1, connect="finite"))
+        if not all_nan[X]:
+            self.acc_curve_x.setData(v[X])
+        else:
+            self.acc_curve_x.setData([])
 
         if not all_nan[Y]:
-            self.p2.clear()
-
-            if median is not None:
-                self.p2.setYRange(-self.g_range, self.g_range)
-
-            self.p2.setGeometry(self.p1.vb.sceneBoundingRect())
-            self.p2.linkedViewChanged(self.p1.vb, self.p2.XAxis)
-            p = pg.PlotCurveItem(v[Y], pen=self.pen2, connect="finite")
-            self.p2.addItem(p)
+            self.acc_curve_y.setData(v[Y])
+        else:
+            self.acc_curve_y.setData([])
 
         if not all_nan[Z]:
-            self.p3.clear()
-
-            if median is not None:
-                self.p3.setYRange(-self.g_range, self.g_range)
-
-            self.p3.setGeometry(self.p1.vb.sceneBoundingRect())
-            self.p3.linkedViewChanged(self.p1.vb, self.p3.XAxis)
-            p = pg.PlotCurveItem(v[Z], pen=self.pen3, connect="finite")
-            self.p3.addItem(p)
+            self.acc_curve_z.setData(v[Z])
+        else:
+            self.acc_curve_z.setData([])
 
 
 class AltitudeGraphWidget(ScreenWidget):
@@ -255,12 +310,11 @@ class AltitudeGraphWidget(ScreenWidget):
     def __init__(self, parent, config):
         s = config.display.resolution
         cols = VERTICAL_ITEMS if s[0] < s[1] else HOLIZONTAL_ITEMS
-        for i, e in enumerate(self.elements):
-            self.item_layout[e] = (i // cols, i % cols)
-        max_height = len(self.elements) // cols
-        self.item_layout[self.__class__.__name__] = (max_height, 0, -1, -1)
+        self.item_layout, plot_y = build_item_layout(
+            self.elements, cols, self.__class__.__name__
+        )
         self.plot_x = 0
-        self.plot_y = max_height
+        self.plot_y = plot_y
 
         super().__init__(parent, config, self.item_layout)
 
@@ -273,6 +327,16 @@ class AltitudeGraphWidget(ScreenWidget):
         self.p2 = pg.ViewBox()
         self.p1.scene().addItem(self.p2)
         self.p2.setXLink(self.p1)
+        self.p1.vb.sigResized.connect(self._sync_overlay_view)
+
+        self.altitude_curve = pg.PlotCurveItem(pen=self.pen1, connect="finite")
+        self.altitude_gps_curve = pg.PlotCurveItem(pen=self.pen2, connect="finite")
+        self.p1.addItem(self.altitude_curve)
+        self.p2.addItem(self.altitude_gps_curve)
+        self.legend = self.p1.addLegend(offset=(8, 8))
+        self.legend.addItem(self.altitude_curve, "Altitude")
+        self.legend.addItem(self.altitude_gps_curve, "Alt.(GPS)")
+        self.legend.setZValue(1000)
 
         plot.setXRange(0, self.config.G_GUI_PERFORMANCE_GRAPH_DISPLAY_RANGE)
         plot.setMouseEnabled(x=False, y=False)
@@ -281,6 +345,12 @@ class AltitudeGraphWidget(ScreenWidget):
         self.y_shift = 0  # self.y_ra  nge * 0.25
 
         self.layout.addWidget(plot, self.plot_y, self.plot_x, -1, -1)
+        self._sync_overlay_view()
+
+    def _sync_overlay_view(self):
+        scene_rect = self.p1.vb.sceneBoundingRect()
+        self.p2.setGeometry(scene_rect)
+        self.p2.linkedViewChanged(self.p1.vb, self.p2.XAxis)
 
     def set_font_size(self, length):
         self.font_size = int(length / 7)
@@ -312,26 +382,20 @@ class AltitudeGraphWidget(ScreenWidget):
             else:
                 self.y_range = 10 * (int(self.y_range / 10) + 1)
 
-            self.p1.clear()
             if median is not None:
                 self.p1.setYRange(median - self.y_range, median + self.y_range)
 
-            self.p1.addItem(
-                pg.PlotCurveItem(v["altitude_graph"], pen=self.pen1, connect="finite")
-            )
+            self.altitude_curve.setData(v["altitude_graph"])
+        else:
+            self.altitude_curve.setData([])
 
         if not all_nan["altitude_gps_graph"]:
-            self.p2.clear()
-
             if median is not None:
                 self.p2.setYRange(
                     median - self.y_range + self.y_shift,
                     median + self.y_range + self.y_shift,
                 )
 
-            self.p2.setGeometry(self.p1.vb.sceneBoundingRect())
-            self.p2.linkedViewChanged(self.p1.vb, self.p2.XAxis)
-            p = pg.PlotCurveItem(
-                v["altitude_gps_graph"], pen=self.pen2, connect="finite"
-            )
-            self.p2.addItem(p)
+            self.altitude_gps_curve.setData(v["altitude_gps_graph"])
+        else:
+            self.altitude_gps_curve.setData([])
