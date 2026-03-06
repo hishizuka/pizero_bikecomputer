c70a6a37835350fb10e47a9b5fa3552cfb3bf075
hishizuka <12926652+hishizuka@users.noreply.github.com>
2026-03-06 22:53:54 +0900

    Improve cuesheet marquee scrolling behavior

--- modules/pyqt/pyqt_cuesheet_widget.py
+++ modules/pyqt/pyqt_cuesheet_widget.py
@@ -18,34 +18,124 @@ class MarqueeLabel(QtWidgets.QLabel):
     def __init__(self, config, parent=None):
         QtWidgets.QLabel.__init__(self, parent)
         self.config = config
-        self.px = 0
+        self.text_margin = 5
+        self.px = self.text_margin
         self.py = 18
         self.timer = QtCore.QTimer(self)
-        self.timer.timeout.connect(self.update)
+        self.timer.timeout.connect(self._advance)
+        self.start_delay_timer = QtCore.QTimer(self)
+        self.start_delay_timer.setSingleShot(True)
+        self.start_delay_timer.timeout.connect(self._start_scrolling)
         self.timer_interval = 200  # [ms]
+        self.start_delay_ms = 2000
         self._speed = 5
         self.textLength = 0
         self.setWordWrap(False)
         self.setStyleSheet(self.STYLES)
 
     def setText(self, text):
+        text = "" if text is None else text
+        if text == self.text():
+            self.refresh_animation_state(reset_position=False)
+            return
         super().setText(text)
-        self.textLength = self.fontMetrics().horizontalAdvance(text)
-        if self.textLength > self.width() and self.config.G_CUESHEET_SCROLL:
-            self.timer.start(self.timer_interval)
+        self.refresh_animation_state(reset_position=True)
+
+    def _has_overflow_text(self):
+        available_width = max(0, self.width() - self.text_margin * 2)
+        return (
+            self.config.G_CUESHEET_SCROLL
+            and bool(self.text())
+            and self.textLength > available_width
+        )
+
+    def _needs_scroll(self):
+        return self.isVisible() and self._has_overflow_text()
+
+    def _set_idle_position(self):
+        self.px = self.text_margin
+
+    def _schedule_scroll_start(self):
+        self.timer.stop()
+        if not self._needs_scroll():
+            self.start_delay_timer.stop()
+            return
+        self.start_delay_timer.start(self.start_delay_ms)
+
+    def _start_scrolling(self):
+        if not self._needs_scroll():
+            return
+        self.timer.start(self.timer_interval)
+
+    def refresh_animation_state(self, reset_position=False):
+        self.textLength = self.fontMetrics().horizontalAdvance(self.text())
+
+        if reset_position:
+            self.timer.stop()
+            self.start_delay_timer.stop()
+            self._set_idle_position()
+
+        if not self._has_overflow_text():
+            self.stop_marquee()
+            return
+
+        if self._needs_scroll():
+            if self.px == self.text_margin:
+                if (
+                    reset_position
+                    or (
+                        not self.start_delay_timer.isActive()
+                        and not self.timer.isActive()
+                    )
+                ):
+                    self._schedule_scroll_start()
+            elif not self.timer.isActive():
+                self.timer.start(self.timer_interval)
+        else:
+            self.timer.stop()
+            self.start_delay_timer.stop()
+
+        self.update()
+
+    def stop_marquee(self):
+        if self.timer.isActive():
+            self.timer.stop()
+        if self.start_delay_timer.isActive():
+            self.start_delay_timer.stop()
+        self._set_idle_position()
+        self.update()
+
+    def _advance(self):
+        if not self._needs_scroll():
+            self.stop_marquee()
+            return
+
+        self.px -= self._speed
+        if self.px <= -self.textLength:
+            self._set_idle_position()
+            self._schedule_scroll_start()
+        self.update()
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self.refresh_animation_state(reset_position=True)
+
+    def showEvent(self, event):
+        super().showEvent(event)
+        self.refresh_animation_state(reset_position=False)
+
+    def hideEvent(self, event):
+        self.stop_marquee()
+        super().hideEvent(event)
 
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
         self.py = int(self.height() * 0.9)
-        if self.textLength <= self.width() or not self.config.G_CUESHEET_SCROLL:
-            painter.drawText(self.px + 5, self.py, self.text())
+        if not self._needs_scroll():
+            painter.drawText(self.text_margin, self.py, self.text())
             return
 
-        if self.px <= -self.fontMetrics().horizontalAdvance(self.text()):
-            self.px = self.width()
         painter.drawText(self.px, self.py, self.text())
-        painter.translate(self.px, 0)
-        self.px -= self._speed
 
 
 class DistanceLabel(QtWidgets.QLabel):
@@ -93,6 +183,8 @@ class CueSheetItem(QtWidgets.QVBoxLayout):
             # q.setStyleStrategy(QtGui.QFont.NoSubpixelAntialias) #avoid subpixel antialiasing on the fonts if possible
             # q.setStyleStrategy(QtGui.QFont.NoAntialias) #don't antialias the fonts
             text.setFont(q)
+            if isinstance(text, MarqueeLabel):
+                text.refresh_animation_state(reset_position=True)
 
 
 class CueSheetWidget(ScreenWidget):
@@ -127,6 +219,16 @@ class CueSheetWidget(ScreenWidget):
         for elem in self.cuesheet:
             elem.reset()
 
+    def start(self):
+        super().start()
+        for elem in self.cuesheet:
+            elem.name.refresh_animation_state(reset_position=False)
+
+    def stop(self):
+        super().stop()
+        for elem in self.cuesheet:
+            elem.name.stop_marquee()
+
     def resizeEvent(self, event):
         self.set_font_size(min(self.size().height(), self.size().width()))
         for i in self.cuesheet:
@@ -153,4 +255,6 @@ class CueSheetWidget(ScreenWidget):
             dist_text = f"{dist / 1000:4.1f}km " if dist > 1000 else f"{dist:6.0f}m  "
             cuesheet_item.dist.setText(dist_text)
             name_text = self.course_points.type[cp_i + i]
+            #name_text = self.course_points.name[cp_i + i]
+            #name_text = self.course_points.notes[cp_i + i]
             cuesheet_item.name.setText(name_text)
